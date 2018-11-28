c22254f5ae1d95eb85b25d1d5e593be5b904e18b
louisotter <louis.otter@mail.mcgill.ca>
2018-11-28 11:52:28 -0500

    Set up local review component

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const parser = require('body-parser');
 const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use(parser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -25,7 +25,8 @@ app.get('/restaurants/*', (req, res) => {
 
 // Reviews Service
 app.get('/API/Reviews/*', (req, res) => {
-  axios.get(`http://34.207.247.29${req.url}`)
+  console.log(req.url, "req.url")
+  axios.get(`http://localhost:3020${req.url}`)
     .then((results) => {
       res.send(results.data);
     })
@@ -37,51 +38,50 @@ app.get('/API/Reviews/*', (req, res) => {
 
 // Overview Service
 app.get('/api/*', (req, res) => {
-  axios.get(`http://3.16.45.212${req.url}`)
-    .then((results) => {
-      res.send(results.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send();
-    });
+  // axios.get(`http://3.16.45.212${req.url}`)
+  //   .then((results) => {
+  //     res.send(results.data);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.send();
+  //   });
 });
 
 // Reservations Service
 app.get('/reservations/*', (req, res) => {
-  axios.get(`http://18.217.247.139${req.url}`)
-    .then((results) => {
-      res.send(results.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send();
-    });
+  // axios.get(`http://18.217.247.139${req.url}`)
+  //   .then((results) => {
+  //     res.send(results.data);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.send();
+  //   });
 });
 
 // Header
 app.get('/header', (req, res) => {
-  axios.get(`http://34.207.247.29:8888${req.url}`)
-    .then((results) => {
-      res.send(results.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send();
-    });
+  // axios.get(`http://34.207.247.29:8888${req.url}`)
+  //   .then((results) => {
+  //     res.send(results.data);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.send();
+  //   });
 });
 
 // Header
 app.post('/header', (req, res) => {
-  console.log(req.body);
-  axios.post(`http://34.207.247.29:8888${req.url}`, {id: req.body.id})
-    .then((results) => {
-      res.send(results.data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send();
-    });
+  // axios.post(`http://34.207.247.29:8888${req.url}`, {id: req.body.id})
+  //   .then((results) => {
+  //     res.send(results.data);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     res.send();
+  //   });
 });
 
 app.listen(port, () => {
